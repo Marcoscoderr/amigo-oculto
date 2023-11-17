2cbd66af24c41ebbf0ce030e821645ca96766ce9
Marcoscoderr <marcos.coder@outlook.com>
2023-11-16 22:04:32 -0300

    Inicio do projeto

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # amigo-oculto
 Software para fazer a separação dos participantes de um amigo oculto
+
+Como utilizar:
+
+1 - Insira o nome e e-mail de cada participante
+
+O sistema fará o sorteio e separação dos participantes e enviará via e-mail...
---
+++ aimgo-oculto.py
@@ -0,0 +1,34 @@
+import random
+
+#Função principal
+def main():
+
+    #Dicionario com o nome e o e-mail dos participantes
+
+    #Lista com o nome dos participantes que posteriomente será embaralhado
+    nome_participantes = []
+
+    #Dicionario com definição final: Chave:Pessoa que dá o presente // Valor: Pessoa que recebe o presente
+
+    #Solicitação que o usuário digite o nome e e-mail de cada participante
+
+    #Misturando a lista
+    def mistura_lista():
+        
+        return
+    
+    #Conferindo se o participante não tirou ele mesmo
+
+    #Conferindo se os participantes já não sairam juntos anteriormente
+
+    #Enviado via e-mail para os participantes informando quem eles devem presentear
+    def envio_email():
+
+        return
+
+
+
+
+#Main inicial
+if __name__ == "__main__":
+    main()
