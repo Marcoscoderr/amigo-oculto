ebdf40d8224b394af801833dc44165d295bd636d
Marcos Silva <marcos.coder@outlook.com>
2023-11-22 00:09:59 -0300

    Alteração de layout; Inserção de menu inicial

--- aimgo-oculto.py
+++ aimgo-oculto.py
@@ -1,4 +1,5 @@
 import random
+from time import sleep
 
 #Função principal
 def main():
@@ -16,63 +17,87 @@ def main():
     novo_participante = ""
 
     #Menu com opções??? (Inserir participante, excluir participante, sortear, sair...)
-
-
-    #Solicitação que o usuário digite o nome e e-mail de cada participante
-    #DEVE SER UMA FUNCAO????
-    while novo_participante != "n":
-        try:
-            nome = input("Digite o nome do participante: ")
-
-            #Tratamento do nome
-
-            email = input(f"Digite o e-mail de {nome}: ")
-
-            #Tratamento do e-mail
-
-            #Inserindo participante no dicionário
-            cadastro_participantes[nome] = email
-            #Inserindo nome do participante na lista que será sorteada
-            nome_participantes.append(nome)
-
-            #TESTE
-            print(cadastro_participantes)
-            print(nome_participantes)
-            print(len(cadastro_participantes))
-
-            novo = input("Gostaria de inserir mais um participante? s-SIM / n-NÃO: ")
-            
-            #Tratamento da escolha - INCOMPLETO
-            novo.lower()
-
-            #Conferir se o usuário digitou somente os caracteres permitidos: "s" ou "n"
-            if novo != "s" and novo != "n":
-                raise ValueError("Digite somente 's' para SIM ou 'n' para NÃO!")
-            #CORRIGIR O ERRO QUE FAZ O COMANDO VOLTAR A PEDIR O USUARIO OUTRO PARTICIPANTE
-            #ELE PODE NÃO QUERER INSERIR MAIS PARTICIPANTES, MAS DIGITOU INCORRETAMENTE
-
-            #Tratamento de erro para que seja inseridos no mínimo 3 participantes
-            if novo =="n" and len(cadastro_participantes) < 3:
-                raise ValueError("Mínimo de 3 participantes!")
-
-        except ValueError as error:
-            print(error)
-            continue
-            
-        
-        #Saída do loop caso a escolha seja "n"
-        if novo == 'n':
-            break
     
-    #TESTE DE MISTURA DA LISTA CHAMANDO A FUNÇÂO MISTURA_LISTA()
-    print(f"Original: {nome_participantes}")
-    mistura_lista(nome_participantes)
-    print(f"Teste mistura 1: {nome_participantes}")
-    mistura_lista(nome_participantes)
-    print(f"Teste mistura 2: {nome_participantes}")
-    mistura_lista(nome_participantes)
-    print(f"Teste mistura 3: {nome_participantes}")
-        
+    while True:
+        print("-"*10 + " Amigo Oculto " + "-"*10)
+        print("""
+            Menu de Acesso:
+
+            1 - Adicionar participante
+            2 - Remover participante
+            3 - Ver lista de participantes
+            4 - Realizar Sorteio!
+
+            5 - Sair  
+            """)
+        escolha = input("Escolha uma opção: ")
+
+        # Opção 5 - Sai do aplicativo
+        if escolha == "5":
+            break
+
+        # Opção 1 - Adicona participante
+        if escolha == "1":
+            dados = adicionar_participante()
+            #Inserindo participante no dicionário
+            #dados[0] -> nome // dados[1] -> email
+            cadastro_participantes[dados[0]] = dados[1]
+            #Inserindo nome do participante na lista que será sorteada
+            nome_participantes.append(dados[0])
+            
+            #TESTE
+            # print(cadastro_participantes)
+            # print(nome_participantes)
+            # print(len(cadastro_participantes))
+
+        # Opção 2 - Remove participante
+        if escolha == "2":
+            if len(nome_participantes) == 0:
+                #Pausa de 2 segundos para o usuário ler a mensgem
+                print("Você ainda não adicionou participantes!")
+                sleep(2)
+                continue
+            else:
+                #Chama a função de tratar o nome e verifica se o nome está na lista
+                eliminar = remover_participante(nome_participantes)
+                if eliminar in nome_participantes:
+                    cadastro_participantes.pop(eliminar)
+                    nome_participantes.remove(eliminar)
+                    print(f"{eliminar} foi retirado da lista!")
+                    #Pausa de 2 segundos para o usuário ler a mensgem
+                    sleep(2)
+
+                    #TESTE
+                    print(cadastro_participantes)
+                    print(nome_participantes)
+                    
+                else:
+                    print("Esse nome não está na lista!")
+                    #Pausa de 2 segundos para o usuário ler a mensgem
+                    sleep(2)
+
+        # Opção 3 - Ver lista de participantes
+        if escolha == "3":
+            print("Lista de Participantes")
+            for pessoa in cadastro_participantes:
+                print(f"Nome: {pessoa} - E-mail: {cadastro_participantes[pessoa]}")
+                #Pausa para leitura do usuário
+                sleep(5)
+
+
+        # Opção 4 - Realizar sorteio
+        if escolha == "4":
+            try:
+                #Tratamento de erro para que seja inseridos no mínimo 3 participantes
+                if len(cadastro_participantes) < 3:
+                    raise ValueError("Mínimo de 3 participantes!")
+                else:
+                    #Realizar sorterio
+                    print("Realizar sorteio")
+            except ValueError as error:
+                print(error)
+                sleep(2)
+           
 
 #Misturando a lista
 def mistura_lista(lista):
@@ -83,6 +108,43 @@ def mistura_lista(lista):
     """    
     random.shuffle(lista)
     return
+
+#Solicitação que o usuário digite o nome e e-mail de cada participante
+def adicionar_participante():
+    """Função para tratar o nome e o e-mail que será adicionado a lista
+
+    Returns:
+        nome: str Nome do participante
+        email : str Email do participante
+    """    
+    nome = input("Digite o nome do participante: ")
+    #Tratamento do nome
+
+    email = input(f"Digite o e-mail de {nome}: ")
+
+    #Tratamento do e-mail
+
+    return nome, email
+
+#Função para remover participante
+def remover_participante(lista):
+    """Função para tratar o nome do participante que será removido
+
+    Args:
+        lista (string): Lista com o nome dos participantes
+
+    Returns:
+        String: Participante a ser removido
+    """    
+    print("Qual participante você deseja remover? ")
+    for nome in lista:
+        print(nome, end="\n")
+    eliminar = input("Digite o nome: ")
+                
+    #Tratamento nome
+
+    return eliminar
+            
 
 #Definição dos participantes
 
